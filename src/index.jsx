57c21b068b5db94dbb8fe0bfdad15a33de33d0f0
glowlh <m.o.gushchina@gmail.com>
2017-12-09 14:08:02 +0300

    fix root file manes

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import App from './App';
+import App from './app';
 import './reset.css';
 import registerServiceWorker from './registerServiceWorker';
 
